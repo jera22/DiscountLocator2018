9e92881915fb8cde41144030ce52c5b2fa023f3a
jera22 <jerkomitric@gmail.com>
2018-12-11 13:03:48 +0100

    ispravak dva vurnebilitya

--- core/src/main/java/com/air/ws/core/DataLoader.java
+++ core/src/main/java/com/air/ws/core/DataLoader.java
@@ -7,26 +7,8 @@ import air18.foi.hr.database.entities.Store;
 
 public abstract class DataLoader {
 
-
-    public ArrayList<Store> getStores() {
-        return stores;
-    }
-
-    public void setStores(ArrayList<Store> stores) {
-        this.stores = stores;
-    }
-
-    public ArrayList<Store> stores;
-
-    public ArrayList<Discount> getDiscounts() {
-        return discounts;
-    }
-
-    public void setDiscounts(ArrayList<Discount> discounts) {
-        this.discounts = discounts;
-    }
-
-    public ArrayList<Discount> discounts;
+   static public ArrayList<Store> stores;
+   static public ArrayList<Discount> discounts;
 
     protected DataLoadedListener mDataLoadedListener;
 
